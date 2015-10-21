29be7d0954eed34f0a7f71ea98546e6c7a5facb0
Sebastián Estrella <sebas007estrella@gmail.com>
2015-10-21 10:27:57 -0500

    Add createTodo persistence function

--- src/ReactHaskell/Persistence.hs
+++ src/ReactHaskell/Persistence.hs
@@ -7,3 +7,6 @@ import ReactHaskell.Types
 
 listTodos :: Connection -> IO [Todo]
 listTodos c = query_ c "SELECT id, text FROM todos"
+
+createTodo :: Connection -> Todo -> IO [Only Integer]
+createTodo c = query c "INSERT INTO todo (text) VALUES (?) RETURNING id"
--- src/ReactHaskell/Types.hs
+++ src/ReactHaskell/Types.hs
@@ -5,14 +5,22 @@ module ReactHaskell.Types where
 
 import Data.Aeson
 import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.ToRow
+import Database.PostgreSQL.Simple.ToField
 
 data Todo = Todo
-  { todoId   :: Int
+  { todoId   :: Integer
   , todoText :: String
-  }
+  } deriving (Eq, Show)
 
 instance FromRow Todo where
   fromRow = Todo <$> field <*> field
+
+instance ToRow Todo where
+  toRow Todo{..} = [toField todoId, toField todoText]
+
+instance FromJSON Todo where
+  parseJSON = undefined
 
 instance ToJSON Todo where
   toJSON Todo{..} = object ["id" .= todoId, "text" .= todoText]
